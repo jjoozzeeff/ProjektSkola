ed38b757b125591780d8301b511f5b8998987102
jjoozzeeff <jozef.faber.zz@gmail.com>
2024-06-06 21:04:53 +0200

    BRBR

--- kosik.php
+++ kosik.php
@@ -70,6 +70,7 @@ if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
         </div>
     <?php endif; ?>
 </div>
+<br><br><br><br><br>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
     $(document).ready(function(){
