78f5dec30c56a45224409596ea10ba734f81e91b
jjoozzeeff <jozef.faber.zz@gmail.com>
2024-06-10 03:34:32 +0200

    male upravy

--- order-success.php
+++ order-success.php
@@ -1,6 +1,6 @@
-<?php
-include "parts/header.php";
+<?php include "parts/header.php"; ?>
 
+<?php
 $orderedItems = isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
 $cartDetails = [];
 
@@ -12,16 +12,29 @@ if (!empty($orderedItems)) {
 $_SESSION['cart'] = [];
 ?>
 
-<div class="container mt-3">
-    <h1>Objednávka bola úspešná!</h1>
-    <p>Ďakujeme za vašu objednávku. Tu sú detaily vašej objednávky:</p>
-    <ul>
-        <?php
-        foreach ($cartDetails as $product) {
-            echo '<li>' . htmlspecialchars($product['name']) . ' - ' . htmlspecialchars($product['price']) . ' €</li>';
-        }
-        ?>
-    </ul>
+<div class="container py-5">
+    <div class="row">
+        <div class="col">
+            <h1 class="display-4">Objednávka bola úspešná!</h1>
+            <p class="lead">Ďakujeme za vašu objednávku. Tu sú detaily vašej objednávky:</p>
+            <ul class="list-group">
+                <?php foreach ($cartDetails as $product) : ?>
+                    <li class="list-group-item">
+                        <div class="row">
+                            <div class="col-md-2">
+                                <img src="<?= $product['image_path'] ?>" alt="<?= $product['name'] ?>" class="img-fluid rounded">
+                            </div>
+                            <div class="col-md-10">
+                                <h5><?= htmlspecialchars($product['name']) ?></h5>
+                                <p><?= htmlspecialchars($product['description']) ?></p>
+                                <p>Cena: <?= htmlspecialchars($product['price']) ?> €</p>
+                            </div>
+                        </div>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </div>
 </div>
 
 <?php include "parts/footer.php" ?>
